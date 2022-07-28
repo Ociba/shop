2fd50cac34f5b6d58e3e81be05a67cf59c4dd787
Ociba <ocibajames@gmail.com>
2022-07-28 20:00:49 +0300

    new offline changges

--- app/Http/Controllers/ItemsWithDiscountApiController.php
+++ app/Http/Controllers/ItemsWithDiscountApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Resources\ItemsWithDiscountResource;
 use App\Models\Shop;
+use DB;
 
 class ItemsWithDiscountApiController extends Controller
 {
@@ -13,5 +14,17 @@ class ItemsWithDiscountApiController extends Controller
     $get_items_with_discount =ItemsWithDiscountResource::collection(Shop::where('status','active')->whereNotNull('discount')
     ->orderBy('created_at','DESC')->get());
     return response()->json(['data'=>$get_items_with_discount]);
+}
+
+/**
+ * This function gets discount calculations
+ */
+private function calculateDiscountAmount(){
+    $percetage =DB::table('shops')->where('discount','!=',null)->value('discount');
+    $percentage_discount =$percetage * 0.01;
+    $product_amount =DB::table('shops')->where('discount','!=',null)->value('price');
+    $calculate_percentage_mount =$product_amount * $percentage_discount;
+    $new_discount_amount =$product_amount-$calculate_percentage_mount;
+    return $new_discount_amount;
 }
 }
